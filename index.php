f5666cd9929e161becb2be21ec31ab61ecde5c23
SheshanAbeysekara <101467343+SheshanAbeysekara@users.noreply.github.com>
2022-03-20 19:08:51 +0530

    Made few edits to menu bars

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#">User Signup</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">User Signin</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Register with Us!</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Customer Sign In</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Admin</a></li>
 
                     </ul>
