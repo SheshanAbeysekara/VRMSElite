103a7ee9bf8bb80a47ea140868f94e8b7258bd63
SheshanAbeysekara <sheshan046@gmail.com>
2022-04-21 23:11:32 +0530

    Changed the DB Connection from MySQ to Heroku ClearDB connect

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -3,9 +3,16 @@ PPA Project - Elite Squad
 Vehicle Repair Management System Website -->
 
 <?php
-$con=mysqli_connect("localhost", "root", "", "vsmsdb");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
+
+$hostname = "us-cdbr-east-05.cleardb.net ";
+$username = "be3f3caab396e7";
+$password = "ac56655c";
+$db = "heroku_db38da5d8dd34df";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if($dbconnect->connect_error) {
+    die("Database connection failed: " . $dbconnect->connect_error);
 }
 
-  ?>
+?>
